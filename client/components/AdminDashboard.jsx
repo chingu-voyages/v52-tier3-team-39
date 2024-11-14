3a830954c0cd685123868404b22700ba68b037e1
Kristi Hwang <kristi.y.l.h@gmail.com>
2024-11-14 13:45:27 -0800

    add customized checklist column to chang e status to 'visited', add logic to handle checkmark

--- client/components/AdminDashboard.jsx
+++ client/components/AdminDashboard.jsx
@@ -3,48 +3,84 @@ import React, { useState } from "react";
 import { DataGrid } from "@mui/x-data-grid";
 
 export default function AdminDashboard() {
-  const rows = [
+  const [hasVisited, setHasVisited] = useState(false);
+  const status_options = ["Requested", "Confirmed", "Pending", "Visited"];
+
+  const [rows, setRows] = useState([
     {
       id: 1,
-      col1: "confirmed",
-      col2: "6a-8a",
-      col3: "Targaryen",
-      col4: "Daenerys",
-      col5: "333-333-3333",
-      col6: "iConquor@westeros.com",
-      col7: "333 Dothraki Valley, Free City of Penthos, Valerya 33333",
+      col1: hasVisited,
+      col2: "Confirmed",
+      col3: "6a-8a",
+      col4: "Targaryen",
+      col5: "Daenerys",
+      col6: "333-333-3333",
+      col7: "iConquor@westeros.com",
+      col8: "333 Dothraki Valley, Free City of Penthos, Valerya 33333",
     },
     {
       id: 2,
-      col1: "pending",
-      col2: "2p-4p",
-      col3: "Lannister",
-      col4: "Tyrion",
-      col5: "031-294-4822",
-      col6: "iDrink@andKnowThings.com",
-      col7: "699 Hand of the King Row, Here and There, Seven Kingdoms 00000",
+      col1: hasVisited,
+      col2: "Pending",
+      col3: "2p-4p",
+      col4: "Lannister",
+      col5: "Tyrion",
+      col6: "031-294-4822",
+      col7: "iDrink@andKnowThings.com",
+      col8: "699 Hand of the King Row, Here and There, Seven Kingdoms 00000",
     },
     {
       id: 3,
-      col1: "requested",
-      col2: "11a-1p",
-      col3: "Stark",
-      col4: "Arya",
-      col5: "909-485-3822",
-      col6: "aGirl@IsNoOne.com",
-      col7: "39842 Northern Way, Many Faced God, Esos 10101",
+      col1: hasVisited,
+      col2: "Requested",
+      col3: "11a-1p",
+      col4: "Stark",
+      col5: "Arya",
+      col6: "909-485-3822",
+      col7: "aGirl@IsNoOne.com",
+      col8: "39842 Northern Way, Many Faced God, Esos 10101",
     },
-  ];
+  ]);
 
   const columns = [
-    { field: "col1", headerName: "Status", width: 150 },
-    { field: "col2", headerName: "Timeslot", width: 150 },
-    { field: "col3", headerName: "Last Name", width: 150 },
-    { field: "col4", headerName: "First Name", width: 150 },
-    { field: "col5", headerName: "Phone", width: 150 },
-    { field: "col6", headerName: "Email", width: 150 },
-    { field: "col7", headerName: "Address", width: 150 },
+    {
+      field: "col1",
+      headerName: "Mark as visited",
+      //   type: "boolean",
+      width: 150,
+      renderCell: (params) => (
+        <input
+          type="checkbox"
+          checked={params.row.visited}
+          onChange={() => handleVisited(params.row.id)}
+        />
+      ),
+    },
+    { field: "col2", headerName: "Status", width: 150 },
+    { field: "col3", headerName: "Timeslot", width: 150 },
+    { field: "col4", headerName: "Last Name", width: 150 },
+    { field: "col5", headerName: "First Name", width: 150 },
+    { field: "col6", headerName: "Phone", width: 150 },
+    { field: "col7", headerName: "Email", width: 150 },
+    { field: "col8", headerName: "Address", width: 150 },
   ];
+
+  const handleVisited = (id) => {
+    setHasVisited(!hasVisited);
+    setRows((prev) =>
+      prev.map((row) =>
+        row.id === id
+          ? {
+              ...row,
+              col1: hasVisited,
+              col2: hasVisited ? "Visited" : row.status,
+            }
+          : row
+      )
+    );
+  };
+
+  const paginationModel = { page: 0, pageSize: 15 };
 
   return (
     <div className="bg-green-100 p-8 rounded-lg shadow-lg">
@@ -52,7 +88,13 @@ export default function AdminDashboard() {
         Reservations:
       </h1>
       <div style={{ height: 300, width: "100%" }}>
-        <DataGrid rows={rows} columns={columns} />
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          initialState={{ pagination: { paginationModel } }}
+          pageSizeOptions={[15, 10]}
+          sx={{ border: 0 }}
+        />
       </div>
     </div>
   );
