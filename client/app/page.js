0588b9b53a947c2c701b9518cb572464c18baccd
Kristi Hwang <kristi.y.l.h@gmail.com>
2024-11-13 17:15:44 -0800

    add sublist for contacts

--- client/app/page.js
+++ client/app/page.js
@@ -1,6 +1,5 @@
 "use client";
 import { Container, Typography } from "@mui/material";
-import AdminDashboard from "../components/AdminDashboard";
 
 export default function Home() {
   return (
@@ -10,7 +9,6 @@ export default function Home() {
         So you say you want a <q>rayvolution</q> ... in affordable solar panel
         installation services specific to the Los Angeles area.
       </Typography>
-      <AdminDashboard />
     </Container>
   );
 }
--- client/components/AdminDashboard.jsx
+++ client/components/AdminDashboard.jsx
@@ -1,23 +1,32 @@
+"use client";
 import { useState } from "react";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
 
 export default function AdminDashboard() {
   return (
-    <div className="bg-green-500 bg-opacity-40 p-8 rounded-lg shadow-lg">
+    <div className="bg-green-500 bg-opacity-70 p-8 rounded-lg shadow-lg">
       <h1 className="text-2xl font-semibold mb-4">Reservations:</h1>
       <div className="flex flex-col gap-3">
-        <div className="bg-white p-4 bg-opacity-40 rounded shadow">
-          <p>
+        <ul className="bg-white p-4 bg-opacity-60 rounded shadow">
+          <li>
             <strong>Status:</strong>
-          </p>
-          <p>
+          </li>
+          <li>
             <strong>Timeslot:</strong>
-          </p>
-          <p>
+          </li>
+          <li>
+            <strong>Name:</strong>
+          </li>
+          <li>
             <strong>Contact:</strong>
-            <p className="mx-4">Email:</p>
-            <p className="mx-4">Phone:</p>
-          </p>
-        </div>
+            <ul>
+              <li className="mx-4">Phone:</li>
+              <li className="mx-4">Email:</li>
+              <li className="mx-4">Address:</li>
+            </ul>
+          </li>
+        </ul>
       </div>
     </div>
   );
