cc99168a678f589e39fc135e36e66bb0456ab639
AyaShiau <63468278+adbshiau@users.noreply.github.com>
2024-11-14 19:12:02 -0800

    all addresses are getting fetched from the API

--- client/components/AddressInput.jsx
+++ client/components/AddressInput.jsx
@@ -1,13 +1,30 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { FormControl, FormHelperText, InputLabel, Input } from "@mui/material";
 
 export default function AddressInput() {
+  const [addresses, setAddresses] = useState([]);
+  // Fetch data from API
+  useEffect(() => {
+    async function fetchAddresses() {
+      try {
+        const response = await fetch(
+          "https://data.lacity.org/resource/4ca8-mxuh.json?"
+        );
+        const data = await response.json();
+        setAddresses(data);
+      } catch (error) {
+        console.error("Error fetching addresses:", error);
+      }
+    }
+    fetchAddresses();
+  }, []);
+
   return (
     <FormControl>
       <InputLabel htmlFor="address-input">Address</InputLabel>
-      <Input id="my-input" aria-describedby="my-helper-text" />
+      <Input id="address-input" aria-describedby="my-helper-text" />
       <FormHelperText id="my-helper-text">
         We'll never share your email.
       </FormHelperText>
