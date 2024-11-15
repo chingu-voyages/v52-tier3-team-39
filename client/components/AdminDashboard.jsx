1dcd670ec488ce57f9ec8725f5473c14c7649633
Kristi Hwang <kristi.y.l.h@gmail.com>
2024-11-15 14:00:11 -0800

    resolve merge conflicts

--- client/components/AdminDashboard.jsx
+++ client/components/AdminDashboard.jsx
@@ -1,52 +1,54 @@
 "use client";
 import React, { useState } from "react";
 import { DataGrid } from "@mui/x-data-grid";
+import { Typography } from "@mui/material";
 
 export default function AdminDashboard() {
-  //   const [hasVisited, setHasVisited] = useState(false);
   const status_options = ["Requested", "Confirmed", "Pending", "Visited"];
 
   const [rows, setRows] = useState([
     {
       id: 1,
-      col1: false,
-      col2: "Confirmed",
-      col3: "6a-8a",
-      col4: "Targaryen",
-      col5: "Daenerys",
-      col6: "333-333-3333",
-      col7: "iConquor@westeros.com",
-      col8: "333 Dothraki Valley, Free City of Penthos, Valerya 33333",
+      visited: false,
+      status: "Confirmed",
+      date: "11/15/24",
+      time: "6a-8a",
+      lastname: "Targaryen",
+      firstname: "Daenerys",
+      phone: "333-333-3333",
+      email: "iConquor@westeros.com",
+      address: "333 Dothraki Valley, Free City of Penthos, Valerya 33333",
     },
     {
       id: 2,
-      col1: false,
-      col2: "Pending",
-      col3: "2p-4p",
-      col4: "Lannister",
-      col5: "Tyrion",
-      col6: "031-294-4822",
-      col7: "iDrink@andKnowThings.com",
-      col8: "699 Hand of the King Row, Here and There, Seven Kingdoms 00000",
+      visited: false,
+      status: "Pending",
+      date: "12/3/24",
+      time: "2p-4p",
+      lastname: "Lannister",
+      firstname: "Tyrion",
+      phone: "031-294-4822",
+      email: "iDrink@andKnowThings.com",
+      address: "699 Hand of the King Row, Here and There, Seven Kingdoms 00000",
     },
     {
       id: 3,
-      col1: false,
-      col2: "Requested",
-      col3: "11a-1p",
-      col4: "Stark",
-      col5: "Arya",
-      col6: "909-485-3822",
-      col7: "aGirl@IsNoOne.com",
-      col8: "39842 Northern Way, Many Faced God, Esos 10101",
+      visited: false,
+      status: "Requested",
+      date: "12/24/24",
+      time: "11a-1p",
+      lastname: "Stark",
+      firstname: "Arya",
+      phone: "909-485-3822",
+      email: "aGirl@IsNoOne.com",
+      address: "39842 Northern Way, Many Faced God, Esos 10101",
     },
   ]);
 
   const columns = [
     {
-      field: "col1",
+      field: "visited",
       headerName: "Mark as visited",
-      //   type: "boolean",
       width: 150,
       renderCell: (params) => (
         <input
@@ -56,24 +58,17 @@ export default function AdminDashboard() {
         />
       ),
     },
-    { field: "col2", headerName: "Status", width: 150 },
-    { field: "col3", headerName: "Timeslot", width: 150 },
-    { field: "col4", headerName: "Last Name", width: 150 },
-    { field: "col5", headerName: "First Name", width: 150 },
-    { field: "col6", headerName: "Phone", width: 150 },
-    { field: "col7", headerName: "Email", width: 150 },
-    { field: "col8", headerName: "Address", width: 150 },
+    { field: "status", headerName: "Status", width: 150 },
+    { field: "date", headerName: "Date", width: 150 },
+    { field: "time", headerName: "Timeslot", width: 150 },
+    { field: "lastname", headerName: "Last Name", width: 150 },
+    { field: "firstname", headerName: "First Name", width: 150 },
+    { field: "phone", headerName: "Phone", width: 150 },
+    { field: "email", headerName: "Email", width: 150 },
+    { field: "address", headerName: "Address", width: 150 },
   ];
 
-  //   React.useEffect(() => {
-  //     function manageVisited() {
-  //       setHasVisited(!hasVisited);
-  //     }
-  //     return manageVisited;
-  //   }, [hasVisited]);
-
   const handleVisited = (id) => {
-    // setHasVisited(!hasVisited);
     setRows((prev) =>
       prev.map((row) =>
         row.id === id
@@ -91,9 +86,12 @@ export default function AdminDashboard() {
 
   return (
     <div className="bg-green-100 p-8 rounded-lg shadow-lg">
-      <h1 className="text-2xl text-stone-800 font-semibold mb-4">
+      <Typography
+        variant="h3"
+        className="text-2xl text-stone-800 font-semibold mb-4"
+      >
         Reservations:
-      </h1>
+      </Typography>
       <div style={{ height: 300, width: "100%" }}>
         <DataGrid
           rows={rows}
