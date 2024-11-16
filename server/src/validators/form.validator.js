90bf69c23cc9849d61e5746722c143368147dc77
Tim Kent <timkentdev@gmail.com>
2024-11-16 12:23:10 -0800

    update validators

--- server/src/validators/form.validator.js
+++ server/src/validators/form.validator.js
@@ -1,18 +1,17 @@
 import Joi from "joi";
 
 const formSchema = Joi.object({
-  name: Joi.string()
-    .pattern(new RegExp(/^[A-Za-z]+$/))
-    .min(2)
-    .max(30)
-    .required(),
-  email: Joi.string().email({
-    minDomainSegments: 2,
-    tlds: { allow: false },
-  }),
+  name: Joi.string().min(2).max(255).required().trim(),
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: false },
+    })
+    .trim(),
   phone: Joi.string()
     .pattern(new RegExp(/^[0-9]*$/))
-    .length(10),
+    .length(10)
+    .trim(),
   address: Joi.string().required(),
   earlyTimeHour: Joi.number().min(9).max(16).required(),
   lateTimeHour: Joi.number()
