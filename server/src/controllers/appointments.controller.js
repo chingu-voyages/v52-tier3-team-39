6627228e2a229952a7c8ad9e655db89bf9e07132
Kristi Hwang <kristi.y.l.h@gmail.com>
2024-11-18 16:43:25 -0800

    update backend

--- server/src/controllers/appointments.controller.js
+++ server/src/controllers/appointments.controller.js
@@ -11,29 +11,3 @@ export async function getAllAppointments(req, res, next) {
     return next({ message: "An internal server error occurred" });
   }
 }
-
-// export async function updateAppointment(req, res, next, id) {
-//   try {
-//     const { id } = appointments.id;
-//     const updateVisited = req.body;
-//     const updatedAppointment = await newAppt.findByIdAndUpdate(
-//       id,
-//       updatedVisited,
-//       {
-//         new: true,
-//         runValidators: true,
-//       }
-//     );
-
-//     if (!updatedAppointment) {
-//       res.status(404);
-//       return next({ message: "Appointment not found" });
-//     }
-
-//     res.status(200).json(updatedAppointment);
-//   } catch (error) {
-//     console.error("Error updating appointment:", error);
-//     res.status(500);
-//     next({ message: "An internal server error occurred" });
-//   }
-// }
--- server/src/routes/appointments.routes.js
+++ server/src/routes/appointments.routes.js
@@ -1,15 +1,9 @@
 import { Router } from "express";
 import mongoose from "mongoose";
-// import { newAppt } from "../controllers/form.controller.js";
-import {
-  getAllAppointments,
-  // updateAppointment,
-} from "../controllers/appointments.controller.js";
+import { getAllAppointments } from "../controllers/appointments.controller.js";
 
 const router = Router();
 
 router.get("/", getAllAppointments);
-
-// router.post("/admin-dashboard", updateAppointment);
 
 export default router;
