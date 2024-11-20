24997543aceea17a3ac6d19e0ccc38eba3d75f37
Tim Kent <timkentdev@gmail.com>
2024-11-19 16:26:11 -0800

    remove async tag

--- client/app/admin-dashboard/page.jsx
+++ client/app/admin-dashboard/page.jsx
@@ -2,7 +2,7 @@ import { Suspense } from "react";
 import { Typography } from "@mui/material";
 import ReservationTable from "../../components/admin_dashboard/ReservationTable";
 
-export default async function AdminDashboardView() {
+export default function AdminDashboardView() {
   return (
     <div>
       <Typography variant="h1">Reservations:</Typography>
--- server/src/controllers/appointments.controller.js
+++ server/src/controllers/appointments.controller.js
@@ -3,7 +3,6 @@ import Form from "../models/form.model.js";
 export async function getAllAppointments(req, res, next) {
   try {
     const appointments = await Form.find();
-    console.log("appts", appointments);
     res.status(200).json(appointments);
   } catch (error) {
     console.error("Error fetching appointments:", error);
