e649ee7defb6d494728620361b9538073c701094
Kristi Hwang <kristi.y.l.h@gmail.com>
2024-11-20 16:26:01 -0800

    display google maps, set dummy addresses for Map and map through it

--- client/app/admin-dashboard/page.jsx
+++ client/app/admin-dashboard/page.jsx
@@ -1,6 +1,7 @@
 import { Suspense } from "react";
 import { Typography } from "@mui/material";
 import ReservationTable from "../../components/admin_dashboard/ReservationTable";
+import Map from "../../components/admin_dashboard/Map";
 
 export default function AdminDashboardView() {
   return (
@@ -8,6 +9,7 @@ export default function AdminDashboardView() {
       <Typography variant="h1">Reservations:</Typography>
       <Suspense fallback={<p>Loading...</p>}>
         <ReservationTable />
+        <Map />
       </Suspense>
     </div>
   );
--- client/components/admin_dashboard/Map.jsx
+++ client/components/admin_dashboard/Map.jsx
@@ -1,30 +1,58 @@
-import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api"
-import React from "react"
+"use client";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import { googleApiKey } from "@/constants";
+import React from "react";
 
-export default function Map(){
-    const containerStyle = {
-        width: "100%",
-        height: "400px",
-    }
-    
-    const startCoords = {
-        //L.A. coords
-        lat: 34.052235, 
-        long: -118.243683,
-    }
-    
-    return(
-        <LoadScript googleMapsApiKey = {gMapKey}>
-            <GoogleMap
-                center={startCoords}
-                zoom={15}
-            >
-            {/*Add markers for all addresses saved in appointments*/}
-            {appointments.map(address=> {
-                return <Marker position={address.zipcode} />
-            })}
-            </GoogleMap>
-        </Loadscript>
-    )
+export default function Map() {
+  const containerStyle = {
+    width: "100%",
+    height: "400px",
+  };
+
+  const startCoords = [
+    {
+      //hollywood bowl
+      lat: 34.112236,
+      lng: -118.339432,
+    },
+    {
+      //Griffith obs.
+      lat: 34.1366,
+      lng: -118.2942,
+    },
+    {
+      //l.a.county museum
+      lat: 34.0639,
+      lng: -118.360565,
+    },
+    {
+      //disneyland
+      lat: 33.812511,
+      lng: -117.918976,
+    },
+    {
+      //pink's
+      lat: 34.0839,
+      lng: -118.3443,
+    },
+    {
+      //venice beach
+      lat: 33.9771,
+      lng: -118.467575,
+    },
+  ];
+
+  return (
+    <LoadScript googleMapsApiKey={googleApiKey}>
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={startCoords[4]}
+        zoom={15}
+      >
+        {startCoords.map((address, index) => (
+          <Marker key={index} position={address} />
+        ))}
+      </GoogleMap>
+    </LoadScript>
+  );
 }
-
