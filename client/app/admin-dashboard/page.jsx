dee9c000f7eb36462c4e04d67b7c9f9d52f65f1b
Kristi Hwang <kristi.y.l.h@gmail.com>
2024-11-27 15:55:29 -0800

    add loader back into AdminDashboardView() with Dashboard()

--- client/app/admin-dashboard/page.jsx
+++ client/app/admin-dashboard/page.jsx
@@ -1,18 +1,25 @@
-// import { Suspense } from "react";
-import { Typography } from "@mui/material";
+import { Suspense } from "react";
+import { CircularProgress, Typography } from "@mui/material";
 import ReservationTable from "../../components/admin_dashboard/ReservationTable";
 import Map from "../../components/admin_dashboard/Map";
 import { fetchAppointments } from "@/actions/form";
 
-export default async function AdminDashboardView() {
+async function Dashboard() {
   const appointments = await fetchAppointments();
+  return (
+    <>
+      <ReservationTable appointments={appointments} />
+      <Map appointments={appointments} />
+    </>
+  );
+}
+export default async function AdminDashboardView() {
   return (
     <div>
       <Typography variant="h1">Reservations:</Typography>
-      {/* <Suspense fallback={<CircularProgress />}> */}
-      <ReservationTable appointments={appointments} />
-      <Map appointments={appointments} />
-      {/* </Suspense> */}
+      <Suspense fallback={<CircularProgress />}>
+        <Dashboard />
+      </Suspense>
     </div>
   );
 }
