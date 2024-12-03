38de5f15b3c923c310d5772cfa2c56085050d172
Chris Daly <chrisdalyenq@gmail.com>
2024-12-03 14:02:28 -0800

    Expose server url to the client

--- client/constants/index.js
+++ client/constants/index.js
@@ -1,5 +1,5 @@
 // Environment variables
-export const serverUrl = process.env.SERVER_URL;
+export const serverUrl = process.env.NEXT_PUBLIC_SERVER_URL;
 export const laCityKey = process.env.NEXT_PUBLIC_LACITY_KEY;
 export const googleApiKey = process.env.NEXT_PUBLIC_GOOGLE_API_KEY;
 export const appointmentsMapId = process.env.NEXT_PUBLIC_GOOGLE_APPOINTMENTS_MAP_ID;
