7cc94465cda76b2217c48ec74c9cf97d6b61b75c
Chris Daly <chrisdalyenq@gmail.com>
2024-12-03 14:02:54 -0800

    Switch autocomplete to use our server

--- client/components/form/AutocompleteAddress.jsx
+++ client/components/form/AutocompleteAddress.jsx
@@ -1,44 +1,32 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import autoComplete from "@tarekraafat/autocomplete.js";
 import { FormControl, InputLabel, Input, FormHelperText } from "@mui/material";
-import { laCityKey, LA_CITY_API_BASE_URL } from "@/constants";
+import { serverUrl } from "@/constants";
 
-export default function Autocomplete({ address, setAddress, errorMsg }) {
-  const [addresses, setAddresses] = useState([]);
+export default function Autocomplete({ setAddress, errorMsg }) {
+  const fetchAddresses = async (query) => {
+    const searchString = encodeURIComponent(query);
+    const response = await fetch(`${serverUrl}addresses/autocomplete?searchString=${searchString}`);
 
-  useEffect(() => {
-    async function fetchAddresses() {
-      const response = await fetch(`${LA_CITY_API_BASE_URL}?$limit=500`, {
-        headers: { "X-App-Token": laCityKey },
-      });
+    const { error, suggestions } = await response.json();
 
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-
-      const data = await response.json();
-
-      const combinedAddresses = data.map((address) => {
-        const combineAddressParts = `${address.hse_nbr} ${address.hse_dir_cd} ${
-          address.str_nm
-        } ${address.str_sfx_cd || ""} ${address.zip_cd}`;
-        return combineAddressParts;
-      });
-
-      console.log(combinedAddresses.slice(0, 10));
-      setAddresses(combinedAddresses);
+    if (error) {
+      console.warn("Failed to fetch autocomplete suggestions");
+      console.warn(error);
+      return [];
     }
-    fetchAddresses();
-  }, []);
+
+    return suggestions;
+  }
 
   useEffect(() => {
     const autoCompleteJS = new autoComplete({
       selector: "#autoComplete",
       placeHolder: "Search for Addresses...",
       data: {
-        src: addresses,
+        src: fetchAddresses,
       },
       resultItem: {
         highlight: true,
@@ -53,7 +41,7 @@ export default function Autocomplete({ address, setAddress, errorMsg }) {
         },
       },
     });
-  }, [addresses]);
+  }, [fetchAddresses]);
 
   return (
     <FormControl>
