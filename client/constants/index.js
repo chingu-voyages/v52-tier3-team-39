3b4cb40a97bce6d63cb9c0ce7a634663f177a2d5
Kristi Hwang <kristi.y.l.h@gmail.com>
2024-12-05 11:36:21 -0800

    refresh mapID

--- client/constants/index.js
+++ client/constants/index.js
@@ -1,4 +1,5 @@
 // Environment variables
 export const serverUrl = process.env.NEXT_PUBLIC_SERVER_URL;
 export const googleApiKey = process.env.NEXT_PUBLIC_GOOGLE_API_KEY;
-export const appointmentsMapId = process.env.NEXT_PUBLIC_GOOGLE_APPOINTMENTS_MAP_ID;
+export const appointmentsMapId =
+  process.env.NEXT_PUBLIC_GOOGLE_APPOINTMENTS_MAP_ID;
