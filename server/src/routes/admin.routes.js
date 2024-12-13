2e3b3a13a2280c45d11e79bd41df48dbfdb57716
Kristi Hwang <kristi.y.l.h@gmail.com>
2024-12-13 08:39:40 -0800

    add convertBatchToMatrix() to send over batch of coords to server

--- server/src/routes/admin.routes.js
+++ server/src/routes/admin.routes.js
@@ -2,6 +2,8 @@ import { Router } from "express";
 
 // GET "/admin-dashboard"
 router.get("/");
+// POST "/admin-dashboard/batch"
+router.post("/:coords/batch", getDistanceMatrix);
 // PATCH "/admin-dashboard/scheduler"
 router.patch("/scheduler", createRoute);
 
