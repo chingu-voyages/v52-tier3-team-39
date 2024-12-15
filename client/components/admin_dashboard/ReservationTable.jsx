6b292342601fc39e70362e396c7a74f512ec1bfc
Kristi Hwang <kristi.y.l.h@gmail.com>
2024-12-15 07:05:42 -0800

    get rid of testing btn for sort-algo

--- client/components/admin_dashboard/ReservationTable.jsx
+++ client/components/admin_dashboard/ReservationTable.jsx
@@ -2,32 +2,10 @@
 
 import Box from "@mui/material/Box";
 import Grid from "./Grid";
-import Button from "@mui/material/Button";
-import { convertBatchToMatrix } from "../../actions/route.js";
 
 export default function ReservationTable({ appointments = [] }) {
-  function createBatch(appointments) {
-    return [...appointments]
-      .sort((a, b) => a.dateCreated - b.dateCreated)
-      .slice(0, 8)
-      .map((appt) => ({
-        id: appt.id,
-        location: appt.location,
-        timeRange: appt.timeRange,
-      }));
-  }
-
-  function handleRoute() {
-    const batch = createBatch(appointments);
-    console.log(batch);
-    convertBatchToMatrix(batch);
-  }
-
   return (
     <Box sx={{ height: "100%", width: "100%" }}>
-      <Button variant="outlined" onClick={handleRoute}>
-        Schedule new batch
-      </Button>
       <Grid rows={appointments} />
     </Box>
   );
