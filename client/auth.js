6eda8501888c8959fc155b59ec9d697845329d91
Tim Kent <timkentdev@gmail.com>
2024-12-22 20:03:45 -0800

    Update auth callback and add auth header

--- client/auth.js
+++ client/auth.js
@@ -8,25 +8,27 @@ export const authOptions = {
     GoogleProviders({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      async profile(profile) {
-        // action to server
-        const res = await fetch(serverUrl + "user", {
-          headers: {
-            "x-user-email": profile.email,
-          },
-        });
-        const role = await res.json();
-        return {
-          id: profile.sub,
-          email: profile.email,
-          role,
-        };
+      profile(profile) {
+        return { id: profile.sub, email: profile.email };
       },
     }),
   ],
   callbacks: {
-    jwt({ token, user }) {
-      if (user) token.role = user.role;
+    async jwt({ token, user, account }) {
+      if (user && account) {
+        const response = await fetch(serverUrl + "user", {
+          headers: {
+            Authorization: `Bearer ${account.access_token}`,
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch user data");
+        }
+
+        const { role } = await response.json();
+        token.role = role;
+      }
       return token;
     },
     session({ session, token }) {
@@ -34,12 +36,12 @@ export const authOptions = {
       return session;
     },
     async redirect({ url, baseUrl }) {
-      return baseUrl + '/login-redirect'
-    }
+      return baseUrl + "/login-redirect";
+    },
   },
   pages: {
     signIn: "/signIn",
-    },
+  },
 };
 
 export default NextAuth(authOptions);
