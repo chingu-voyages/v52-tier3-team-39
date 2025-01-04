f90e19ac49269d39cb8377430e603c5d1dd3cc8e
Kristi Hwang <kristi.y.l.h@gmail.com>
2025-01-03 22:11:57 -0800

    fix window is undefined error: initialize isDark with bool, move window and localStorage logic into useEffect

--- client/components/layout/DarkMode.jsx
+++ client/components/layout/DarkMode.jsx
@@ -6,23 +6,28 @@ import DarkModeTwoToneIcon from "@mui/icons-material/DarkModeTwoTone";
 import { IconButton } from "@mui/material";
 
 export default function DarkMode() {
-  const darkModePreferred = window.matchMedia(
-    "(prefers-color-scheme: dark)"
-  ).matches;
-  const darkModeSelected = localStorage.getItem("theme") === "dark";
-  const [isDark, setIsDark] = useState(darkModeSelected || darkModePreferred);
+  const [isDark, setIsDark] = useState(false);
+
+  useEffect(() => {
+    const darkModePreferred = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+    const darkModeSelected = localStorage.getItem("theme") === "dark";
+    setIsDark(darkModeSelected || darkModePreferred);
+  }, []);
 
   useEffect(() => {
     if (isDark) {
       document.documentElement.classList.add("dark");
-    } else document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
   }, [isDark]);
 
   const handleClick = () => {
     setIsDark((prev) => !prev);
-    isDark
-      ? localStorage.setItem("theme", "light")
-      : localStorage.setItem("theme", "dark");
   };
 
   return (
