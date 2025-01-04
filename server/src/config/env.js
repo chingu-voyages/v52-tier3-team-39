00f2af9358a37ba117d0fe3745106e853032c630
Kristi Hwang <kristi.y.l.h@gmail.com>
2025-01-03 21:31:46 -0800

    update graphHopper api key

--- server/src/config/env.js
+++ server/src/config/env.js
@@ -7,3 +7,4 @@ export const dbConnectStr = process.env.DATABASE_CONNECTION_STRING;
 export const myTestDb = process.env.MY_TEST_DB;
 export const mockEmailUser = process.env.MOCK_EMAIL_USER;
 export const mockEmailPass = process.env.MOCK_EMAIL_PASS;
+export const graphHopperApiKey = process.env.GRAPH_HOPPER_API_KEY;
